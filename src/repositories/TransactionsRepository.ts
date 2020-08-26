601dfd051674d8db28064059fd47b0fe4b981d44
Roberto Rocha Rodrigues <rock.bnu@gmail.com>
2020-08-25 22:33:36 -0300

    Desafio 05: Primeiro projeto Node.js
    - Listagem de transações
    - Criação de transação com validações

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,34 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.transactions.reduce(
+      (acc, curr) => {
+        if (curr.type === 'income') {
+          acc.income += curr.value;
+          acc.total += curr.value;
+        }
+        if (curr.type === 'outcome') {
+          acc.outcome += curr.value;
+          acc.total -= curr.value;
+        }
+        return acc;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (['income', 'outcome'].includes(type)) {
+      if (type === 'outcome') {
+        const balance = this.transactionsRepository.getBalance();
+        if (value > balance.total) {
+          throw Error('Transaction outcome greater then total balance');
+        }
+      }
+      return this.transactionsRepository.create({ title, value, type });
+    }
+    throw Error('Type should be income or outcome');
   }
 }
 
